2f87553975737918193ba95d8efae31620f9f2c8
Administrator <Administrator@niit-mr-25.NIITANDHERI.COM>
2019-11-08 08:46:15 +0530

    add product done

--- src/main/java/com/niit/controller/ProductController.java
+++ src/main/java/com/niit/controller/ProductController.java
@@ -1,14 +1,41 @@
 package com.niit.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.niit.dao.ProductDAO;
+import com.niit.model.Product;
+
+//
 @Controller
 @RequestMapping("/product")
 public class ProductController 
 {
-	@RequestMapping("/add")
-	public void addProduct()
+	@Autowired
+	ProductDAO productDAO;
+	
+	@GetMapping("/add")
+	public String addProduct(ModelMap map)
+	{
+		map.addAttribute("product",new Product());
+		return "addproduct";
+	}
+
+	@PostMapping("/add")
+	public String storeProduct(@ModelAttribute ("product") Product product)
+	{
+		
+		productDAO.addProduct(product);
+		return "redirect:/product/display";
+	}
+	
+	@RequestMapping("/display")
+	public void displayProduct()
 	{
 		
 	}
